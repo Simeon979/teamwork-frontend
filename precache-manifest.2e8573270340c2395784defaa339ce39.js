1c82da2b14317312e3da560832b923e751b4d3f7
Simeon <adegbolasimeon@gmail.com>
2019-11-30 00:38:28 +0100

    Updates

---
+++ precache-manifest.2e8573270340c2395784defaa339ce39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416be21b7acbf5e60840e1fc60fa9f01",
+    "url": "/Simeon979/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "54ac245ca75c435582f5",
+    "url": "/Simeon979/teamwork-frontend/static/css/2.da529cc7.chunk.css"
+  },
+  {
+    "revision": "71f20ee9087d9153dcd5",
+    "url": "/Simeon979/teamwork-frontend/static/css/main.1ed30db2.chunk.css"
+  },
+  {
+    "revision": "54ac245ca75c435582f5",
+    "url": "/Simeon979/teamwork-frontend/static/js/2.73e0bb87.chunk.js"
+  },
+  {
+    "revision": "71f20ee9087d9153dcd5",
+    "url": "/Simeon979/teamwork-frontend/static/js/main.f4cb6e54.chunk.js"
+  },
+  {
+    "revision": "821d2bf4fe7501831e87",
+    "url": "/Simeon979/teamwork-frontend/static/js/runtime-main.9dfb9593.js"
+  }
+]);
